0b041520ac81e1de3ee0915916317289f42b25ef
usamah abdul matin <44694889+eexvuu@users.noreply.github.com>
2025-05-22 00:31:09 +0700

    fix: disable eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // ...compat.extends("next/core-web-vitals", "next/typescript"), // Disabled all extended rules
 ];
 
 export default eslintConfig;
